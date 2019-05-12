77cac1b098091b5e1320557fb4ed7116447fd558
Santosh Jadhav <santoshnj79@gmail.com>
2019-05-12 13:05:46 +0530

    created helloworld program

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class HelloWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class HelloWorld
+{
+    public static void main(String[] args){
+        for(int i=1;i<=10;i++){
+            System.out.println("Hello World..." +i);
+        }
+    }
+}
